2c6a4ffd66fcf59959cd98694e5fcc0bd2ce5cf1
Pedro H R C <106977723+1221pepo1221@users.noreply.github.com>
2024-09-23 12:41:38 -0300

    Add files via upload

--- senha.js
+++ senha.js
@@ -1,3 +1,9 @@
 alert("bem vindos ao jogo secreto?");
 let numeroSecreto = 6;
 let chute = prompt("escolha um número entre 1 a 10");
+
+if(chute== numeroSecreto){
+    console.log("isso ai vc acertou!")
+} else{
+    alert("voce é burro!!")
+}
