5a4289fee87de24fc1437e4cbb9ce9e4cb225f7c
Pedro H R C <106977723+1221pepo1221@users.noreply.github.com>
2024-10-21 12:42:45 -0300

    Add files via upload

--- senha.js
+++ senha.js
@@ -1,9 +1,23 @@
 alert("bem vindos ao jogo secreto?");
 let numeroSecreto = 6;
 let chute = prompt("escolha um número entre 1 a 10");
+let tentativas =1;
+while (chute != numeroSecreto) {
+    chute = prompt("Escolha um número entre 1 a 10?");
 
 if(chute== numeroSecreto){
-    console.log("isso ai vc acertou!")
+    console.log("isso ai vc acertou! com ${tentativas}");
 } else{
-    alert("voce é burro!!")
+    if(numeroSecreto> chute){
+        alert("voce é burro!! o numero é maior que ${chute}");
+    }else{
+        alert("voce é burro!! o numero é menor que ${chute}");
+    }
+    
+} tentativas++;
+}
+if (tentativas> 1){
+    alert("isso mesmo suin otário, finalmente!!!! com 1 tentativa");
+}else{
+    alert("isso mesmo suin otário, finalmente!!!! com ${tentativas}");
 }
